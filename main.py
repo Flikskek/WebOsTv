f9ef57d38f3f6a21b37a3b30b170afce73985d07
Flikskek <zyzyfyk@gmail.com>
2021-04-29 22:17:33 +0300

    gui

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
+from tkinter import * 
+
+from pywebostv.discovery import *    # Because I'm lazy, don't do this.
+from pywebostv.connection import *
+from pywebostv.controls import *
+
+def volup():
+    media.volume_up()
+
+def voldown():
+    media.volume_down()
+
+def chup():
+    tv_control.channel_up()
+
+def chdown():
+    tv_control.channel_down()
+
+def poff():
+    system.power_off()
+
+def notify():
+    system.notify('Hello :D')
 
 
+root = Tk()
+root.title("LGTV")
+ 
+vup = Button(text="Volume up", command=volup)
+vdown = Button(text="Volume down", command=voldown)
+chup = Button(text="Channel up", command=chup)
+chdown = Button(text="Channel down", command=chdown)
+poff = Button(text="Power Off", command=poff)
+notify = Button(text="Test Notify", command=notify)
+                           
+
+chdown.pack()
+chup.pack()
+vdown.pack()
+vup.pack()
+poff.pack()
+notify.pack()
+
+store = {'client_key': '28e9be6ce7def461b62d31331e6d901d'}
 
 
+client = WebOSClient("192.168.0.104")
+client.connect()
+
+media = MediaControl(client)
+tv_control = TvControl(client)
+system = SystemControl(client)
+
+
+for status in client.register(store):
+    if status == WebOSClient.PROMPTED:
+        print("Please accept the connect on the TV!")
+    elif status == WebOSClient.REGISTERED:
+        print("Registration successful!")
+
+root.mainloop()
