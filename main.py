4e93565f3a33f10ed78ec8ee0e41a0c570b72e05
Fliks <zyzyfyk@gmail.com>
2021-07-16 18:22:45 +0300

    backup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from tkinter import * 
 
-
-
-from store import tvkey,ip
-
-import commands as com
+from base import tvkey,ip
 
 import time
 
@@ -13,16 +9,35 @@ from pywebostv.connection import *
 from pywebostv.controls import *
 
 
+def volup():
+    media.volume_up()
+
+def voldown():
+    media.volume_down()
+
+def chup():
+    tv_control.channel_up()
+
+
+def chdown():
+    tv_control.channel_down()
+
+def poff():
+    system.power_off()
+
+def notify():
+    system.notify('Hello :D')
+
 root = Tk()
 root.title("LGTV")
 root.geometry("485x600")
 
-vup = Button(text="Volume up", command=com.volup, background="#555", foreground="#ccc")
-vdown = Button(text="Volume down", command=com.voldown, background="#555", foreground="#ccc")
-chup = Button(text="Channel up", command=com.chup, background="#555", foreground="#ccc")
-chdown = Button(text="Channel down", command=com.chdown, background="#555", foreground="#ccc")
-poff = Button(text="Power Off", command=com.poff, background="#555", foreground="#ccc")
-notify = Button(text="Test Notify", command=com.notify ,background="#555", foreground="#ccc")
+vup = Button(text="Volume up", command=volup, background="#555", foreground="#ccc")
+vdown = Button(text="Volume down", command=voldown, background="#555", foreground="#ccc")
+chup = Button(text="Channel up", command=chup, background="#555", foreground="#ccc")
+chdown = Button(text="Channel down", command=chdown, background="#555", foreground="#ccc")
+poff = Button(text="Power Off", command=poff, background="#555", foreground="#ccc")
+notify = Button(text="Test Notify", command=notify ,background="#555", foreground="#ccc")
                            
 
 chdown.place(x=400,y=250)
@@ -33,15 +48,12 @@ poff.place(x=225,y=0)
 notify.place(x=225,y=550)
 
 store = tvkey #TV key
-
-
 client = WebOSClient(ip)
 client.connect()
 
 media = MediaControl(client)
 tv_control = TvControl(client)
 system = SystemControl(client)
-
 
 for status in client.register(store):
     if status == WebOSClient.PROMPTED:
